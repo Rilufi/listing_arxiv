8302dcb053eaff6f3a2415518ab059c9b766168b
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-01-14 16:29:14 -0300

    Update user_settings.py

--- user_settings.py
+++ user_settings.py
@@ -1,3 +1,17 @@
+import tweepy
+import os
+
+consumer_key = os.environ["CONSUMER_KEY"]
+consumer_secret = os.environ["CONSUMER_SECRET"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Controls Twitter account
+api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+
 URL = "https://arxiv.org/list/astro-ph/new"
 
 list_key = {"PHOTO-Zs":["photometric redshift", "photo-z", "photometric redshifts", "photo-zs"],
@@ -6,5 +20,3 @@ list_key = {"PHOTO-Zs":["photometric redshift", "photo-z", "photometric redshift
                 "ML": ["machine learning", "deep learning"],
                 "CLUSTERS": ["cluster"],
                 "AGBs": ["agbs", "agb", "assymptotic giant branch"]}
-
-email = 'YOUR_EMAIL_HERE@gmail.com'
