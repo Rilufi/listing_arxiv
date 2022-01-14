2ee8d23b7c5b08cf97a1b4f346f385781e8dbbaf
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-01-14 16:28:45 -0300

    Update listing_arxiv.py

--- listing_arxiv.py
+++ listing_arxiv.py
@@ -8,7 +8,11 @@ import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from apiclient import errors, discovery
-from user_settings import URL, list_key, email
+from user_settings import URL, list_key, api
+import datetime
+
+today = datetime.date.today() # ex 2015-10-31
+data = today.strftime("%d/%m")
 
 url_get = requests.get(URL)
 soup = BeautifulSoup(url_get.content, 'html.parser')
@@ -47,64 +51,38 @@ for key in list_key:
         email_body = email_body + "No articles published today. \n\n"
 
 
-SCOPES = 'https://www.googleapis.com/auth/gmail.send'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Gmail API quickstart'
+toReply = "rilufix" 
 
-def get_credentials():
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'gmail-python-email-send.json')
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        credentials = tools.run_flow(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
-
-def SendMessage(sender, to, subject, msgHtml, msgPlain):
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
-    message1 = CreateMessageHtml(sender, to, subject, msgHtml, msgPlain)
-    result = SendMessageInternal(service, "me", message1)
-    return result
-
-def SendMessageInternal(service, user_id, message):
-    try:
-        message = (service.users().messages().send(userId=user_id, body=message).execute())
-        print('Message Id: %s' % message['id'])
-        return message
-    except errors.HttpError as error:
-        print('An error occurred: %s' % error)
-        return "Error"
-    return "OK"
-
-def CreateMessageHtml(sender, to, subject, msgHtml, msgPlain):
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = to
-    msg.attach(MIMEText(msgPlain, 'plain'))
-    msg.attach(MIMEText(msgHtml, 'html'))
-    raw = base64.urlsafe_b64encode(msg.as_bytes())
-    raw = raw.decode()
-    return {'raw': raw}
-
-def send_mail():
-    to = email
-    sender = email
-    subject = "Your ArXiv list today"
-    msgHtml = email_body.replace("\n", "<br>")
-    msgPlain = email_body
-    SendMessage(sender, to, subject, msgHtml, msgPlain)
+mystring = f""" Your ArXiv list today {data}"""
 
 
-if __name__ == "__main__":
-    if send:
-        send_mail()
+try:
+    api.update_status(mystring)
+except:
+    api.update_status("Não rolou.")
+
+myexstring = f"""{email_body}"""
+
+
+def get_chunks(s, maxlength):
+    start = 0
+    end = 0
+    while start + maxlength  < len(s) and end != -1:
+        end = s.rfind(" ", start, start + maxlength + 1)
+        yield s[start:end]
+        start = end +1
+    yield s[start:]
+
+chunks = get_chunks(myexstring, 280)
+
+#Make list with line lengths:
+chunkex = [(n) for n in chunks]
+
+coun = 0
+
+
+while coun < len(chunkex):
+    tweets = api.user_timeline(screen_name = toReply, count=1)
+    for tweet in tweets:
+        api.update_status(str(chunkex[coun]), in_reply_to_status_id = tweet.id, auto_populate_reply_metadata = True)
+        coun += 1
